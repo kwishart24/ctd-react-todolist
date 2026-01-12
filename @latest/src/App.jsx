039b473f9f586880e561c178abc2bffb519a1ca7
Kristen Wishart <kwishart24@gmail.com>
2026-01-11 20:42:16 -0500

    adding useEffect

--- @latest/src/App.jsx
+++ @latest/src/App.jsx
@@ -1,10 +1,55 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import TodoList from './features/TodoList/TodoList.jsx';
 import TodoForm from './features/TodoForm.jsx';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+
+  const token = `Bearer ${import.meta.env.VITE_PAT}`;
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      setIsLoading(true);
+
+      const options = {
+        method: 'GET',
+        headers: {
+          Authorization: token,
+        },
+      };
+
+      try {
+        const resp = await fetch(url, options);
+
+        if (!resp.ok) {
+          throw new Error(resp.message);
+        }
+        const data = await resp.json();
+
+        const todos = data.records.map((record) => {
+          const todo = {
+            id: record.id,
+            title: record.fields.title || '',
+            isCompleted: record.fields.isCompleted || false,
+          };
+          return todo;
+        });
+
+        setTodoList(todos);
+      } catch (error) {
+        setErrorMessage(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchTodos();
+  }, []);
 
   const addTodo = (title) => {
     const newTodo = {
@@ -20,7 +65,7 @@ function App() {
       if (todo.id === editedTodo.id) {
         return {
           //if it matches - return a new object that destructures the editedTodo
-          ...editedTodo
+          ...editedTodo,
         };
       }
       return todo;
@@ -32,7 +77,12 @@ function App() {
     <div>
       <h1>My Todos</h1>
       <TodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} setTodoList={setTodoList} onUpdateTodo={updateTodo}/>
+      <TodoList
+        todoList={todoList}
+        setTodoList={setTodoList}
+        onUpdateTodo={updateTodo}
+        isLoading={isLoading}
+      />
     </div>
   );
 }
