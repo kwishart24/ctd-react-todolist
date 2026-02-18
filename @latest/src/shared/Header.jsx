7cf57c6bfdace0a3f30791355f14f999a44ab874
Kristen Wishart <kwishart24@gmail.com>
2026-02-18 18:16:13 -0500

    working on Header

--- @latest/src/shared/Header.jsx
+++ @latest/src/shared/Header.jsx
@@ -1,6 +1,38 @@
+import { NavLink } from 'react-router';
+import styles from './Header.module.css';
 
-function ({title}) {
-    return <h1>{title}</h1>
+function Header({ title }) {
+  return (
+    <header>
+      <h1>{title}</h1>
+      <nav>
+        <NavLink
+          to="/"
+          className={({ isActive }) => {
+            if (isActive === true) {
+              return styles.active;
+            } else {
+              return styles.inactive;
+            }
+          }}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) => {
+            if (isActive === true) {
+              return styles.active;
+            } else {
+              return styles.inactive;
+            }
+          }}
+        >
+          About
+        </NavLink>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
